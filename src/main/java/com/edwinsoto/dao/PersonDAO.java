23e6a7e30e803cde1107dcffa10e1431d2be0012
Edwin Soto <edwin.soto-clark@expeditors.com>
2024-04-16 09:25:38 -0400

    Completed spring app up to week 7

---
+++ src/main/java/com/edwinsoto/dao/PersonDAO.java
@@ -0,0 +1,23 @@
+package com.edwinsoto.dao;
+
+import com.edwinsoto.adopter.Adopter;
+
+import java.util.Collection;
+import java.util.List;
+public interface PersonDAO {
+
+    Adopter insert(Adopter person);
+
+    boolean update(Adopter person);
+
+    boolean delete(int id);
+
+    Adopter findByID(int id);
+
+    boolean findByName(String name);
+
+    Collection<Adopter> naturalSort();
+
+    List<Adopter> findAll();
+
+}
